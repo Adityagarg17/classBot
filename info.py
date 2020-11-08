30c9b5f102c4a44652dab9f71e16e8990f46c3f8
Adityagarg17 <74129829+Adityagarg17@users.noreply.github.com>
2020-11-08 13:04:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,19 +1,17 @@
 #Only for storing links and timings... Supported apps=> Zoom, Google Meet, Microsoft Teams and Cisco WebX
 #completed Final Version
 
-firstClass = ("08:30")
-secondClass = ("09:10")
-thirdClass = ("09:50")
-fourthClass = ("10:30")
-fifthClass = ("11:15")
-dummyclass = ("13:00")
+firstClass = ("07:55")
+secondClass = ("08:50")
+thirdClass = ("09:40")
+fourthClass = ("10:40")
+fifthClass = ("12:15")
 
-#Subscribe = "www.youtube.com/HasanImam"
 
 #enter links here
-english = ("https://zoom.")
-maths = ("https://zoom.")
-biology = ("https://zoom.")
-physics = ("https://us04web.zoom.")
-chemistry = ("https://us04web.zoom.")
-dummy = ("https://us05web.zoom.")
+english = ("https://teams.microsoft.com/l/team/19%3af95973801e834e85a54030c07ef2b7be%40thread.tacv2/conversations?groupId=6cc96103-3853-4d3a-88f9-67ebc0bc1ef7&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
+maths = ("https://teams.microsoft.com/l/team/19%3ab764fadf8e524c04a2512246ccd5850a%40thread.tacv2/conversations?groupId=07f7ef80-517b-4abc-80cf-319f7276e0f0&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
+bst = ("https://teams.microsoft.com/l/team/19%3abe66e7d7ab0b4ab3a7d07005a0bb539a%40thread.tacv2/conversations?groupId=c9e8c679-fd0f-4764-993f-9fdca80cc1f1&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
+accounts = ("https://teams.microsoft.com/l/team/19%3a35ba27bfe5254f22b506602a9a4a851a%40thread.tacv2/conversations?groupId=1db83564-b33c-4e00-b0e9-7da4fde9e71b&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
+economics = ("https://teams.microsoft.com/l/team/19%3a8774526aabaf4425a3721fcfae8b4d41%40thread.tacv2/conversations?groupId=4a60941e-037a-4327-bc76-ca790b843b50&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
+ecodef = ("https://teams.microsoft.com/l/team/19%3a3490bd2e841742a5b0bc8de70f002ad0%40thread.tacv2/conversations?groupId=cacd231c-dbfd-4be0-8165-a7d9be8173a1&tenantId=5ec1d34a-bff6-4916-abf6-aee75a35aae1")
